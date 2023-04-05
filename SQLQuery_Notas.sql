04b2ae8d145d0d9870832fb69a718fa789c0231e
naiaragabriela <naigabi31@gmail.com>
2023-04-05 14:47:59 -0300

    criacao trigger item matricula

--- SQLQuery_Notas.sql
+++ SQLQuery_Notas.sql
@@ -1,6 +1,7 @@
 create DATABASE CONTROLE_ACADEMICO;
 
-CREATE TABLE Aluno(
+CREATE TABLE Aluno
+(
     RA int NOT NULL,
     Nome varchar (100) NOT NULL,
 
@@ -8,7 +9,8 @@ CREATE TABLE Aluno(
 );
 GO
 
-CREATE TABLE Disciplina(
+CREATE TABLE Disciplina
+(
     Codigo INT NOT NULL,
     Nome VARCHAR(50) NOT NULL,
     CargaHoraria INT NOT NULL,
@@ -17,7 +19,8 @@ CREATE TABLE Disciplina(
 );
 GO
 
-Create Table Matricula(
+Create Table Matricula
+(
     ID INT identity (1,1),
     RA int NOT NULL,
     Ano INT NOT NULL,
@@ -29,7 +32,8 @@ Create Table Matricula(
 );
 GO
 
-Create Table Item_Matricula(
+Create Table Item_Matricula
+(
     ID int NOT NULL,
     Codigo INT NOT NULL,
     Nota_1 DECIMAL (4,2),
@@ -47,29 +51,50 @@ GO
 
 --inserção de alunos 
 
-Insert into Aluno values (1,'Giovani');
-Insert into Aluno (Nome,RA) VALUES ('Ana Maria',2);
-insert into Aluno values (3, 'Felipe');
+Insert into Aluno
+values
+    (1, 'Giovani');
+Insert into Aluno
+    (Nome,RA)
+VALUES
+    ('Ana Maria', 2);
+insert into Aluno
+values
+    (3, 'Felipe');
 
-select * from Aluno
-order by Nome desc 
+select *
+from Aluno
+order by Nome desc
 
 
 --inserção de disciplinas
 
-insert into Disciplina VALUES (1,'Banco de dados', 80), 
-(2,'IA', 80), (3,'SO', 60);
-select * from Disciplina
+insert into Disciplina
+VALUES
+    (1, 'Banco de dados', 80),
+    (2, 'IA', 80),
+    (3, 'SO', 60);
+select *
+from Disciplina
 
 --inserção de matricula
 
-insert into Matricula VALUES (1, 2023, 1);
-SELECT * from Matricula;
+insert into Matricula
+VALUES
+    (1, 2023, 1);
+SELECT *
+from Matricula;
 
-insert into Matricula VALUES (2, 2023, 1);
-SELECT * from Matricula;
-insert into Matricula VALUES (3, 2023, 1);
-SELECT * from Matricula;
+insert into Matricula
+VALUES
+    (2, 2023, 1);
+SELECT *
+from Matricula;
+insert into Matricula
+VALUES
+    (3, 2023, 1);
+SELECT *
+from Matricula;
 
 -- alteração da disciplina
 
@@ -81,93 +106,110 @@ where codigo = 1
 
 delete Disciplina
 where codigo = 3
-select * from Disciplina
+select *
+from Disciplina
 
 --Disciplinas da Matricula de cada Aluno
 
-    insert into Item_Matricula (id, codigo, faltas, situacao) values (1, 1, 0, 'matriculado')
-    SELECT * from Item_Matricula;
-    insert into Item_Matricula (id, codigo, faltas, situacao) values (1, 2, 0, 'matriculado')
-    insert into Item_Matricula (id, codigo, faltas, situacao) values (2, 1, 0, 'matriculado')
-    SELECT * from Item_Matricula;
-    SELECT * FROM Aluno;
+insert into Item_Matricula
+    (id, codigo, faltas, situacao)
+values
+    (1, 1, 0, 'matriculado')
+SELECT *
+from Item_Matricula;
+insert into Item_Matricula
+    (id, codigo, faltas, situacao)
+values
+    (1, 2, 0, 'matriculado')
+insert into Item_Matricula
+    (id, codigo, faltas, situacao)
+values
+    (2, 1, 0, 'matriculado')
+SELECT *
+from Item_Matricula;
+SELECT *
+FROM Aluno;
 
-    select m.Ano, m.Semestre, m.id, a.Nome, d.Nome 
-    from Aluno a JOIN Matricula m on a.RA = m.RA 
+select m.Ano, m.Semestre, m.id, a.Nome, d.Nome
+from Aluno a JOIN Matricula m on a.RA = m.RA
     JOIN Item_Matricula im on m.ID = im.ID
     JOIN Disciplina d on im.Codigo = d.Codigo
 
 
-    select m.Ano, m.Semestre, m.id, a.Nome, d.Nome, im.Nota_1, im.Nota_2, im.Sub, im.Faltas
-    from Aluno a JOIN Matricula m on a.RA = m.RA 
+select m.Ano, m.Semestre, m.id, a.Nome, d.Nome, im.Nota_1, im.Nota_2, im.Sub, im.Faltas
+from Aluno a JOIN Matricula m on a.RA = m.RA
     JOIN Item_Matricula im on m.ID = im.ID
     JOIN Disciplina d on im.Codigo = d.Codigo
-    where a.nome = 'Giovani'
+where a.nome = 'Giovani'
 
 
 --Cadastros de Notas na tabela 
 
-    UPDATE Item_Matricula SET Nota_1 = 9, Nota_2 = 7, Sub = Null
+UPDATE Item_Matricula SET Nota_1 = 9, Nota_2 = 7, Sub = Null
     Where Id = 1 and Codigo = 2
-    SELECT * from Item_Matricula;
-    UPDATE Item_Matricula SET Nota_1 = 4, Nota_2 = 5, Sub = 6
+SELECT *
+from Item_Matricula;
+UPDATE Item_Matricula SET Nota_1 = 4, Nota_2 = 5, Sub = 6
     Where Id = 1 and Codigo = 1
-    SELECT * from Item_Matricula;
-    UPDATE Item_Matricula SET Nota_1 = 4, Nota_2 = 2, Sub = 8
+SELECT *
+from Item_Matricula;
+UPDATE Item_Matricula SET Nota_1 = 4, Nota_2 = 2, Sub = 8
     Where Id = 2 and Codigo = 1
-    SELECT * from Item_Matricula;
+SELECT *
+from Item_Matricula;
 
 
-    select m.Ano, m.Semestre, m.id, a.Nome, d.Nome, im.Nota_1, im.Nota_2, im.Sub, im.Faltas
-    from Aluno a JOIN Matricula m on a.RA = m.RA 
+select m.Ano, m.Semestre, m.id, a.Nome, d.Nome, im.Nota_1, im.Nota_2, im.Sub, im.Faltas
+from Aluno a JOIN Matricula m on a.RA = m.RA
     JOIN Item_Matricula im on m.ID = im.ID
     JOIN Disciplina d on im.Codigo = d.Codigo
-    where a.nome = 'Giovani'
+where a.nome = 'Giovani'
 
 
 
 --calculo da media usando select
 
-    SELECT m.Ano, m.Semestre, m.id, a.Nome, d.Nome, im.Nota_1, im.Nota_2, im.Sub, im.Faltas,
+SELECT m.Ano, m.Semestre, m.id, a.Nome, d.Nome, im.Nota_1, im.Nota_2, im.Sub, im.Faltas,
     CASE 
     WHEN (Sub is Null) then (Nota_1 + Nota_2)/2
     WHEN (Sub > Nota_1) and (Nota_1 < Nota_2) then (Sub + Nota_2)/2
     WHEN (Sub > Nota_2) and (Nota_2 < Nota_1) then (Sub+ Nota_1)/2
     END AS 'Media'
-    FROM Aluno a JOIN Matricula m ON a.RA = m.RA
+FROM Aluno a JOIN Matricula m ON a.RA = m.RA
     JOIN Item_Matricula im on m.ID = im.ID
-    JOIN Disciplina d ON im.Codigo = d.Codigo 
-    Where a.Nome = 'Giovani'
+    JOIN Disciplina d ON im.Codigo = d.Codigo
+Where a.Nome = 'Giovani'
 
 
 --cadastro de faltas na tabela 
 
-    UPDATE Item_Matricula SET Faltas = 20
+UPDATE Item_Matricula SET Faltas = 20
     Where Id = 1 and Codigo = 2
-    UPDATE Item_Matricula SET Faltas = 40
+UPDATE Item_Matricula SET Faltas = 40
     Where Id = 1 and Codigo = 1
-    UPDATE Item_Matricula SET Faltas = 70
+UPDATE Item_Matricula SET Faltas = 70
     Where Id = 2 and Codigo = 1
-    SELECT * from Item_Matricula;
+SELECT *
+from Item_Matricula;
 
 --calculo reprovacao por falta usando apenas select
 
-    SELECT m.Ano, m.Semestre, m.id, a.Nome, d.Nome, im.Nota_1, im.Nota_2, im.Sub, im.Faltas,
+SELECT m.Ano, m.Semestre, m.id, a.Nome, d.Nome, im.Nota_1, im.Nota_2, im.Sub, im.Faltas,
     CASE 
     WHEN (CargaHoraria*0.25<Faltas) then 'Reprovado por falta'
     WHEN (CargaHoraria*0.25>Faltas) then 'Aprovado por falta, ver media'
     END AS 'Situação'
-    FROM Aluno a JOIN Matricula m ON a.RA = m.RA
+FROM Aluno a JOIN Matricula m ON a.RA = m.RA
     JOIN Item_Matricula im on m.ID = im.ID
-    JOIN Disciplina d ON im.Codigo = d.Codigo 
-    Where a.Nome = 'Ana Maria'
+    JOIN Disciplina d ON im.Codigo = d.Codigo
+Where a.Nome = 'Ana Maria'
 
 
 --adicionando uma coluna na tabela 
 
 ALTER TABLE Item_Matricula ADD 
     Media DECIMAL (4,2)
-   GO 
+   GO
 
 
 --criacao trigger para calcular media depois de uma insercao
@@ -175,25 +217,93 @@ ALTER TABLE Item_Matricula ADD
 CREATE OR ALTER TRIGGER TGR_Media_Update ON Item_Matricula AFTER UPDATE
 AS 
 BEGIN
-    DECLARE @Id INT, @Codigo INT, @Nota_1 DECIMAL, @Nota_2 DECIMAL, @Media DECIMAL
+    IF (UPDATE(Nota_2))
+    BEGIN
+        DECLARE @Id INT, @Codigo INT, @Nota_1 DECIMAL(4,2), @Nota_2 DECIMAL(4,2), @Media DECIMAL (4,2)
 
-    SELECT @Id = Id, @Codigo = Codigo,  @Nota_1 = Nota_1, @Nota_2 = Nota_2 FROM inserted
+        SELECT @Id = Id, @Codigo = Codigo, @Nota_1 = Nota_1, @Nota_2 = Nota_2
+        FROM inserted
 
-    SET @Media  = (@Nota_1 + @Nota_2)/2
+        SET @Media  = (@Nota_1 + @Nota_2)/2
 
-    UPDATE Item_Matricula SET Media = @Media
+        UPDATE Item_Matricula SET Media = @Media
 
-    WHERE Id = @Id And Codigo = @Codigo
-
+        WHERE Id = @Id And Codigo = @Codigo
+    END
 END;
+GO
 
-    
-INSERT Item_Matricula (Id, Codigo, Faltas, Situacao ) Values (3,2,0,'matriculado')
-INSERT Item_Matricula (Id, Codigo, Faltas, Situacao ) Values (3,1,0,'matriculado')
+
+--insercao de novos valores
+
+INSERT Item_Matricula
+    (Id, Codigo, Faltas, Situacao )
+Values
+    (3, 2, 0, 'matriculado')
+INSERT Item_Matricula
+    (Id, Codigo, Faltas, Situacao )
+Values
+    (3, 1, 0, 'matriculado')
 
 UPDATE Item_Matricula SET Nota_1 = 6, Nota_2 = 8 WHERE Id = 3 AND Codigo = 2
 UPDATE Item_Matricula SET Nota_1 = 6, Nota_2 = 8 WHERE Id = 3 AND Codigo = 1
 
 
 SELECT * FROM Item_Matricula
-    
+
+GO
+
+
+--criar uma trigger que vai definir a situação do aluno baseado na media
+
+CREATE OR ALTER TRIGGER TGR_Situacao_Update ON Item_Matricula AFTER UPDATE
+As
+BEGIN
+    IF(UPDATE(Media))
+    BEGIN
+        DECLARE @Id INT, @Codigo INT, @Media DECIMAL (4,2), @Situacao VARCHAR (19)
+
+        SELECT @Id = Id, @Codigo = Codigo, @Media = Media, @Situacao = Situacao
+        FROM inserted
+
+        SET @Situacao = CASE 
+    WHEN (@Media >= 5) THEN 
+    'Aprovado'
+    ELSE
+    'Reprovado' 
+    END
+        PRINT (@situacao)
+
+        UPDATE Item_Matricula SET Situacao = @Situacao
+    Where Id = @ID AND Codigo = @Codigo
+    END
+END;
+GO
+UPDATE Item_Matricula SET Nota_1 = 5, Nota_2 = 4 WHERE Id = 3 AND Codigo = 2
+UPDATE Item_Matricula SET Nota_1 = 3, Nota_2 = 8 WHERE Id = 3 AND Codigo = 1
+GO
+--criar uma Trigger que Verifica a quantidade de faltas se a quantidade de faltas do aluno na 
+--materia for superior a metade da carga horaria ele esta reprovado automaticamente
+
+CREATE OR ALTER TRIGGER TGR_Faltas_Update ON Item_Matricula AFTER UPDATE
+As
+BEGIN
+    IF(UPDATE(Faltas))
+    BEGIN
+        DECLARE @Id INT, @Codigo INT, @CargaHoraria INT, @Faltas INT, @Situacao VARCHAR (19)
+
+        SELECT @Id = Id, @Codigo = Codigo, @Faltas = Faltas FROM inserted
+        SELECT @CargaHoraria = CargaHoraria FROM Disciplina where Codigo = @Codigo
+
+        SET @Situacao = CASE
+    WHEN (@Faltas > @CargaHoraria/2) THEN 
+    'Reprovado por falta'
+    END
+    UPDATE Item_Matricula SET Situacao = @Situacao
+    Where Id = @ID AND Codigo = @Codigo
+    END
+END;
+GO
+
+UPDATE Item_Matricula SET Faltas = 71 WHERE Id = 2 AND Codigo = 1
+SELECT * FROM Item_Matricula
